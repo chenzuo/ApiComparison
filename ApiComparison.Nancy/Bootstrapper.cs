7e17156045f7e86f4bdf1d47d4b55da5407bf46c
Mattias Nordberg <mattias.nordberg@mpsbroadband.com>
2013-02-20 15:35:22 +0100

    Update nancy and turn off CSRF

--- ApiComparison.Nancy/Bootstrapper.cs
+++ ApiComparison.Nancy/Bootstrapper.cs
@@ -1,10 +1,16 @@
 ﻿using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.Security;
 
 namespace ApiComparison.Nancy
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(global::Nancy.TinyIoc.TinyIoCContainer container, IPipelines pipelines)
+        {
+            Csrf.Disable(pipelines);
+        }
+
         protected override NancyInternalConfiguration InternalConfiguration
         {
             get
--- ApiComparison.NancyAsync/Bootstrapper.cs
+++ ApiComparison.NancyAsync/Bootstrapper.cs
@@ -1,10 +1,18 @@
 ﻿using Nancy;
 using Nancy.Bootstrapper;
+using Nancy.Security;
 
 namespace ApiComparison.NancyAsync
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            Csrf.Disable(pipelines);
+        }
+
         protected override NancyInternalConfiguration InternalConfiguration
         {
             get
